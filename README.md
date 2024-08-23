ee26878b7c1bbfaf76527548b97e42ebefbc80f1
bhavya-abaqus <bhavya@abaq.us>
2024-08-23 22:01:33 +0530

    new

---
+++ README.md
@@ -0,0 +1,22 @@
+# [ExcelJS](https://www.npmjs.com/package/exceljs) + [Playwright](https://playwright.dev/)
+## Build excel utils &amp; drive uploads downloads using Playwright &amp; ExcelJS NPM package.
+### Read, write, and manipulate spreadsheet data and styles to XLSX and JSON files programmatically, using a simple and intuitive API
+
+***
+Initialize npm project (package.json file will be created)
+
+    npm init
+
+Install excelJS package/plugin in npm project (node_modules folder will be created with all excelJS dependencies & also save installed package as dependencies in package.json file) [So anyone can import & use project directly]
+
+    npm install exceljs
+
+Installs a package as a development dependency and adds it to the package.json file (To update or re-install only the dev dependencies, you must use the --save-dev flag)
+
+    npm i <package name> --save-dev 
+    npm i <pkg> -d
+
+To run js file
+
+    node excelDemo.js
+***
---
+++ excelDemo.js
@@ -0,0 +1,26 @@
+const excel = require('exceljs'); // Import class from excelJS package/library
+
+const workbook = new excel.Workbook(); // Creating new object of exceljs class & invoking its method | It hold collection of sheets
+workbook.xlsx.readFile("resources/excelTest.xlsx").then(function () {  // Handel the promise 
+    const worksheet = workbook.getWorksheet('Sheet1'); // It hold details of entire worksheet
+
+    worksheet.eachRow((row, rowNumber) => { // Outer loop
+        row.eachCell((cell, colNumber) => { // Inner loop
+            console.log(cell.value);
+        })
+    })
+})
+
+// Another way - by using await
+async function excelTest() {
+    const workbook = new excel.Workbook(); // Creating new object of exceljs class & invoking its method | It hold collection of sheets
+    await workbook.xlsx.readFile("resources/excelTest.xlsx");  // Wait until this operation is completed then proceed to next
+    const worksheet = workbook.getWorksheet('Sheet1'); // It hold details of entire worksheet
+
+    worksheet.eachRow((row, rowNumber) => { // Outer loop
+        row.eachCell((cell, colNumber) => { // Inner loop
+            console.log(cell.value);
+        })
+    })
+}
+excelTest();
