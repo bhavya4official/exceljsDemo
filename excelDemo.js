b03bd4d0948aa1fce31f948bea97e0e8c694012f
bhavya-abaqus <bhavya@abaq.us>
2024-08-24 00:09:01 +0530

    comments

--- excelDemo.js
+++ excelDemo.js
@@ -1,9 +1,10 @@
+/* Read excel file */
 const excel = require('exceljs'); // Import class from excelJS package/library
 
 const workbook = new excel.Workbook(); // Creating new object of exceljs class & invoking its method | It hold collection of sheets
 workbook.xlsx.readFile("resources/excelTest.xlsx").then(function () {  // Handel the promise 
     const worksheet = workbook.getWorksheet('Sheet1'); // It hold details of entire worksheet
-
+    /* Traversing rows & columns of excel worksheet */
     worksheet.eachRow((row, rowNumber) => { // Outer loop
         row.eachCell((cell, colNumber) => { // Inner loop
             console.log(cell.value);
